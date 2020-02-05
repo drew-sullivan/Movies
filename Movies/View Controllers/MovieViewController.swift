7b18f5d9fa0d5cbea7456b3f28739b75c2f975cc
Drew Sullivan <dsullivan@allegion.com>
2020-02-04 21:59:24 -0700

    Added custom screen sizing to cv

--- Movies/View Controllers/MovieViewController.swift
+++ Movies/View Controllers/MovieViewController.swift
@@ -54,3 +54,25 @@ class MovieViewController: UIViewController {
     }
     
 }
+
+extension MovieViewController: UICollectionViewDelegateFlowLayout {
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let height = view.frame.size.height
+        let width = view.frame.size.width
+        let padding = 5
+
+        var numCellsInRow: Int
+        var cellWidth: CGFloat
+        let cellHeight: CGFloat
+        if height > width {
+            numCellsInRow = 2
+        } else {
+            numCellsInRow = 4
+        }
+        cellWidth = (width / CGFloat(numCellsInRow)) - CGFloat(padding)
+        cellHeight = cellWidth * 3.2 / 2
+        return CGSize(width: cellWidth, height: cellHeight)
+    }
+
+}
